1e07ba9f180491d9ed270eedb0a03446ca92e423
Agaano <121362942+Agaano@users.noreply.github.com>
2023-12-09 22:12:09 +0300

    Update BurgerMenu.tsx

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -4,24 +4,36 @@ const menu = [
 	{ title: 'Контакты', src: '#' },
 	{ title: 'Форма заявки', src: '#' },
 ]
-export default ({ open }: { open: boolean }) => {
+export default ({
+	open,
+	setOpen,
+}: {
+	open: boolean
+	setOpen: (open: boolean) => void
+}) => {
 	return (
-		<menu className={`${open ? 'opened' : 'closed'}`}>
-			<div className='content'>
-				<nav>
-					<ul>
-						{menu.map(({ title, src }) => {
-							return (
-								<li>
-									<a href={src}>
-										<span>{title}</span>
-									</a>
-								</li>
-							)
-						})}
-					</ul>
-				</nav>
-			</div>
-		</menu>
+		<>
+			<menu className={`${open ? 'opened' : ''}`}>
+				<div className='content'>
+					<nav>
+						<ul>
+							{menu.map(({ title, src }) => {
+								return (
+									<li>
+										<a href={src}>
+											<span>{title}</span>
+										</a>
+									</li>
+								)
+							})}
+						</ul>
+					</nav>
+				</div>
+			</menu>
+			<div
+				className={`menu_overlay ${open ? 'opened' : ''}`}
+				onClick={() => setOpen(false)}
+			></div>
+		</>
 	)
 }
